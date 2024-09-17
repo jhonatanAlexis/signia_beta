348439ad3f7cceec4010ae18589c3040b0251a21
jhonatan <jhonatan24100@gmail.com>
2024-09-17 17:57:54 -0600

    tercer commit

--- app.py
+++ app.py
@@ -14,6 +14,29 @@ jwt = JWTManager(app)
 init_db(app)
 
 #endpoint para registrar a un usuario
+@app.route('/registrar', methods=['POST'])
+def registrar():
+    data = request.get_json()
+    nombre = data.get('nombre')
+    email = data.get('email')
+    password = data.get('password')
+
+    if mongo.db.users.find_one({
+        'email': email
+    }):
+        return jsonify({'message': 'El usuario ya existe'}), 400
+    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    result = mongo.db.users.insert_one({
+        'nombre': nombre,
+        'email': email,
+        'password': hashed_password
+    })
+    if result.acknowledged:
+        return jsonify({'message': 'Usuario creado con exito'}), 200
+    else:
+        return jsonify({'message': 'Error al crear el usuario'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
