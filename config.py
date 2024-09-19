b200655195a503582340f5afeced74e56fef6ad5
jhonatan <jhonatan24100@gmail.com>
2024-09-18 20:52:54 -0600

    otro commit

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
 
 class ConfigGmail:
     MAIL_SERVER = os.getenv("GMAIL_SERVER")
-    MAIL_PORT = int(os.getenv("GMAIL_PORT", 587))
+    MAIL_PORT = int(os.getenv("GMAIL_PORT"))
     MAIL_USE_TLS = os.getenv("GMAIL_USE_TLS") == 'True'
     MAIL_USERNAME = os.getenv("GMAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
@@ -17,7 +17,7 @@ class ConfigGmail:
 
 class ConfigOutlook:
     MAIL_SERVER = os.getenv("OUTLOOK_SERVER")
-    MAIL_PORT = int(os.getenv("OUTLOOK_PORT", 587))
+    MAIL_PORT = int(os.getenv("OUTLOOK_PORT"))
     MAIL_USE_TLS = os.getenv("OUTLOOK_USE_TLS") == 'True'
     MAIL_USERNAME = os.getenv("OUTLOOK_USERNAME")
     MAIL_PASSWORD = os.getenv("OUTLOOK_PASSWORD")
