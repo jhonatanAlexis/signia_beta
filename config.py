ec3834b5ba7d65de8ab1b1f95512baf5724a0e1a
jhonatan <jhonatan24100@gmail.com>
2024-09-18 20:41:17 -0600

    aaa

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
 
 class ConfigGmail:
     MAIL_SERVER = os.getenv("GMAIL_SERVER")
-    MAIL_PORT = int(os.getenv("GMAIL_PORT"))
+    MAIL_PORT = int(os.getenv("GMAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("GMAIL_USE_TLS") == 'True'
     MAIL_USERNAME = os.getenv("GMAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
@@ -17,8 +17,8 @@ class ConfigGmail:
 
 class ConfigOutlook:
     MAIL_SERVER = os.getenv("OUTLOOK_SERVER")
-    MAIL_PORT = int(os.getenv("OUTLOOK_PORT"))
+    MAIL_PORT = int(os.getenv("OUTLOOK_PORT", 587))
     MAIL_USE_TLS = os.getenv("OUTLOOK_USE_TLS") == 'True'
     MAIL_USERNAME = os.getenv("OUTLOOK_USERNAME")
     MAIL_PASSWORD = os.getenv("OUTLOOK_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("OUTLOOK_DEFAULT_SENDER") 
+    MAIL_DEFAULT_SENDER = os.getenv("OUTLOOK_DEFAULT_SENDER")
